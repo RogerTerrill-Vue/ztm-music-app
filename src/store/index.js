315af4d9ad6fc0fe47cec048673364b96e05ead2
Roger Dodger <lpaddikt@gmail.com>
2022-02-08 23:02:14 -0800

    Add Validation Plugin

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,14 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    authModalShow: false,
   },
   mutations: {
+    toggleAuthModal: (state) => {
+      state.authModalShow = !state.authModalShow;
+    },
+  },
+  getters: {
   },
   actions: {
   },
