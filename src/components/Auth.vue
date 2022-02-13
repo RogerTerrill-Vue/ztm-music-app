4a4c69653be6bd7f387b78bc975358705af913cd
Roger Dodger <lpaddikt@gmail.com>
2022-02-13 10:12:39 -0800

    Init commit

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -64,14 +64,16 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <form v-show="tab === 'register'">
+          <VeeForm v-show="tab === 'register'" :validation-schema="schema">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
-              <input type="text"
+              <VeeField type="text" name="name"
                      class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
-                     placeholder="Enter Name"/>
+                     placeholder="Enter Name"
+              />
+              <ErrorMessage class="text-red-600" name="name" />
             </div>
             <!-- Email -->
             <div class="mb-3">
@@ -125,7 +127,7 @@
                 hover:bg-purple-700">
               Submit
             </button>
-          </form>
+          </VeeForm>
         </div>
       </div>
     </div>
@@ -140,6 +142,15 @@ export default {
   data() {
     return {
       tab: 'login',
+      schema: {
+        name: 'required|min:3|max:100|alpha_spaces',
+        email: '',
+        age: '',
+        password: '',
+        confirm_password: '',
+        country: '',
+        tos: '',
+      },
     };
   },
   computed: {
